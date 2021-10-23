d5e861579db8a85506f8375bbc172be9347d4e3c
Paul <pauldewilson@gmail.com>
2021-10-23 13:56:28 +0100

    changed sql_connection_string to url.create method, documentation updated

--- py/modules/settings.py
+++ py/modules/settings.py
@@ -1,11 +1,22 @@
 """
 Global settings file
 """
+from sqlalchemy.engine import URL
 
 # SQL Server Connection Properties
 SQL_HOST = 'DESKTOP-8TLT5OK\SQLEXPRESS' # your server name here (open SSMS, top of Object Explorer)
 SQL_DATABASE = 'ChinaBank' # or your database that you prefer to use
 SQL_USERNAME = 'pylogin'  # username per the setup documentation
 SQL_PASSWORD = 'pypass' # password per the setup documentation
-SQL_CONNECTION_STRING = f'mssql+pyodbc://{SQL_USERNAME}:{SQL_PASSWORD}\
-    @{SQL_HOST}/{SQL_DATABASE}?driver=SQL Server' # full connection string
+SQL_CONNECTION_STRING = URL.create(
+    "mssql+pyodbc",
+    username=SQL_USERNAME,
+    password=SQL_PASSWORD,
+    host=SQL_HOST,
+    port=1433,
+    database=SQL_DATABASE,
+    query={
+        "driver":"SQL Server",
+        "authentication": "ActiveDirectoryIntegrated",
+    }
+)
